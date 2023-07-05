7cd9471950e582c9ce59bfa228e05070071e3a60
NKeddie <nate.keddie@gmail.com>
2023-07-05 16:24:55 -0400

    Fixing UI for large dataset

--- FloppyNet.UI/floppy-net/src/views/UserView.vue
+++ FloppyNet.UI/floppy-net/src/views/UserView.vue
@@ -2,20 +2,20 @@
   <Suspense>
     <LineChart
       :data="{
-        labels: $store.state.userStats.data.map((_, i) => `1`),
+        labels: bucketize($store.state.userStats.data, d => d.Average, average),
         datasets: [{
           label: 'Average',
           backgroundColor: '#6200EE',
-          data: $store.state.userStats.data.map(u => u.Average)
+          data: bucketize($store.state.userStats.data, d => d.Average, average)
         }, {
           label: 'Max',
           backgroundColor: '#B00020',
-          data: $store.state.userStats.data.map(u => u.Max)
+          data: bucketize($store.state.userStats.data, d => d.Max, max)
         },
         {
           label: 'Min',
           backgroundColor: '#03DAC6',
-          data: $store.state.userStats.data.map(u => u.Min)
+          data: bucketize($store.state.userStats.data, d => d.Min, min)
         }]
       }"
       :options="{
@@ -55,6 +55,37 @@ export default defineComponent({
   components: {
     GameList,
     LineChart
+  },
+  methods: {
+    average: (data) => data.reduce((a, b) => a + b) / data.length,
+    max: (data) => data.reduce((a, b) => a > b ? a : b),
+    min: (data) => data.reduce((a, b) => a < b ? a : b),
+    bucketize(data, selector, aggregationFunction) {
+      const bucketSize = 60
+
+      const itemsPerBucket = Math.round(data.length / bucketSize)
+
+      if (itemsPerBucket <= 1)
+        return data.map(selector);
+
+      var result = [];
+      var index = 0;
+
+      for (var i = 0; i < bucketSize; i++) {
+        var temp = []
+        for (var j = 0; j < itemsPerBucket; j++) {
+          if (index < data.length) {
+            temp.push(data[index])
+            index++;
+          }
+        }
+        if (temp.length > 0) {
+          result.push(aggregationFunction(temp.map(selector)))
+        }
+      }
+
+      return result
+    }
   },
   created: function() {
     this.userId = this.$route.params.id;
